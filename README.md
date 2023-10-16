7ea7da92a7292672423a0c4e80987ff8e0d01c15
phuongnguyen139 <bapcaisach@gmail.com>
2023-10-17 00:34:38 +0700

    refactor the main code get factorial

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 ### You will find in this repo the following stuff
 * The Math-Util library that offers multiple math functions
 * Unit Testing source code with JUnit framework
+* GitHub Actions
 * ...
 
 ### Screenshots
--- src/com/nvxp/mathutil/core/MathUtil.java
+++ src/com/nvxp/mathutil/core/MathUtil.java
@@ -13,19 +13,46 @@ public class MathUtil {
     //Hàm giai thừa dốc đứng, tăng nhanh về giá trị
     //20! gồm 18 con số 0, vừa kịp đủ cho kiểu long của Java
     //Bài này quy ước tính n! từ 0 đến 20
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0 to 20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//
+//        long product = 1; // tích nhân dồn
+//        for (int i = 1; i <= n; i++) {
+//            product *= i;
+//        }
+//
+//        return product;
+//    }
+    
+    //Đệ quy - Recursion
+    //Hiện tượng gọi lại chính mình với 1 quy mô khác
+    //Tính 6!
+    //6! = 6 x 5!
+    //5! = 5 x 4!
+    //4! = 4 x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1! Điểm dừng
+    //1! = 1 rồi hồi ngược lại
+    
+    // Quy ước 1! = 0! = 1
+    
+    //Chốt: n! = n * (n - 1)!
+    
     public static long getFactorial(int n) {
+        
         if (n < 0 || n > 20) {
-            throw new IllegalArgumentException("Invalid argument. N must be between 0 to 20");
+            throw new IllegalArgumentException("N must be between 0..20");
         }
+        
         if (n == 0 || n == 1) {
             return 1;
         }
-
-        long product = 1; // tích nhân dồn
-        for (int i = 1; i <= n; i++) {
-            product *= i;
-        }
-
-        return product;
+        
+        return n * getFactorial(n - 1);
     }
 }
