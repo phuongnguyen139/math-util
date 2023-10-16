169341d3cbe528d2b5f1a504c953c250f65822bb
phuongnguyen139 <bapcaisach@gmail.com>
2023-10-17 00:39:43 +0700

    fix exception message

--- src/com/nvxp/mathutil/core/MathUtil.java
+++ src/com/nvxp/mathutil/core/MathUtil.java
@@ -46,7 +46,7 @@ public class MathUtil {
     public static long getFactorial(int n) {
         
         if (n < 0 || n > 20) {
-            throw new IllegalArgumentException("N must be between 0..20");
+            throw new IllegalArgumentException("Invalid argument. N must be between 0 to 20");
         }
         
         if (n == 0 || n == 1) {
